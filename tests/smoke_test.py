1a3a347505be76edb678b2eb2848a036324c0564
houthacker <houthacker@pm.me>
2026-07-07 06:38:42 +0000

    Add release workflow and smoke test.

---
+++ tests/smoke_test.py
@@ -0,0 +1,12 @@
+"""Test that required files have been included in a build."""
+
+
+def test_smoke():
+    """Test that the API can be imported."""
+    from aio_remeha_modbus.api import const
+
+    assert const.MetaRegisters.NUMBER_OF_DEVICES.start_address == 128
+
+
+if __name__ == "__main__":
+    test_smoke()
