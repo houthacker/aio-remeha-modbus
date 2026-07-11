48ace65a13602f74a274752414cda72df99da920
houthacker <houthacker@pm.me>
2026-07-11 16:20:13 +0000

    Provide aio_remeha_modbus.api.api classes through aio_remeha_modbus.api.

--- src/aio_remeha_modbus/api/__init__.py
+++ src/aio_remeha_modbus/api/__init__.py
@@ -1,1 +1,17 @@
 """Package containing all Remeha Modbus API classes."""
+
+__all__ = [
+    "DeviceBoardCategory",
+    "DeviceBoardType",
+    "DeviceInstance",
+    "RemehaApi",
+    "SerialConnectionMethod",
+]
+
+from .api import (
+    DeviceBoardCategory,
+    DeviceBoardType,
+    DeviceInstance,
+    RemehaApi,
+    SerialConnectionMethod,
+)
